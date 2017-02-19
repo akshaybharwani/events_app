81a5d81798511de68cdfa2c2e9668e2d440c1a5c
Akshay <akshaybharwani@gmail.com>
2017-02-19 12:50:49 +0530

    Remove .idea

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   resources :events
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root 'events#index'
 
   devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks', :registrations => 'users/registrations'}
-  resources :events
   get '/users/:id', :to => 'users#show', :as => :user
   resources :users do
     member do
